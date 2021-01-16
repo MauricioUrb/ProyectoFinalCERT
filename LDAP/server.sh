d480d53e2a4c25191d10f24c59b30b4981f558c3
MauricioUrb <maurbina96@hotmail.com>
2021-01-16 13:20:13 -0600

    dominio temporal

--- LDAP/server.sh
+++ LDAP/server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sudo echo "10.10.20.130	LDAP.fam.local LDAP" >> /etc/hosts
+#sudo echo "10.10.20.130	ldap.dominio.com ldap" >> /etc/hosts
 # Es necesario agregar la dirección IP y el dominio al hosts
 sudo apt-get install slapd ldap-utils ldapscripts -y
 sudo dpkg-reconfigure slapd
@@ -8,8 +8,8 @@ sudo dpkg-reconfigure slapd
 https://devconnected.com/how-to-setup-openldap-server-on-debian-10/
 Datos a introducir
 No
-LDAPS.fam.local
-fam.local
+ldap.dominio.com
+dominio.com
 hola123.,
 MDB
 No
@@ -17,14 +17,14 @@ Yes
 END
 
 echo "Creando archivo base de ldap..."
-echo "dn: ou=People,dc=LDAPS,dc=fam,dc=local
+echo "dn: ou=People,dc=ldap,dc=dominio,dc=com
 objectClass: organizationalUnit
 ou: People" > base.ldif
-sudo ldapadd -x -D cn=admin,dc=LDAPS,dc=fam,dc=local -W -f base.ldif
+sudo ldapadd -x -D cn=admin,dc=ldap,dc=dominio,dc=com -W -f base.ldif
 
 echo "Creando archivos de usuarios de ldap..."
 #Archivos de usuarios
-echo "dn: uid=alberto,ou=People,dc=LDAPS,dc=fam,dc=local
+echo "dn: uid=alberto,ou=People,dc=ldap,dc=dominio,dc=com
 objectClass: inetOrgPerson
 objectClass: posixAccount
 objectClass: shadowAccount
@@ -39,7 +39,7 @@ loginShell: /bin/bash
 gecos: alberto
 description: Alberto" > alberto.ldif
 
-echo "dn: uid=fernando,ou=People,dc=LDAPS,dc=fam,dc=local
+echo "dn: uid=fernando,ou=People,dc=ldap,dc=dominio,dc=com
 objectClass: inetOrgPerson
 objectClass: posixAccount
 objectClass: shadowAccount
@@ -54,7 +54,7 @@ loginShell: /bin/bash
 gecos: fernando
 description: Fernando" > fernando.ldif
 
-echo "dn: uid=mauricio,ou=People,dc=LDAPS,dc=fam,dc=local
+echo "dn: uid=mauricio,ou=People,dc=ldap,dc=dominio,dc=com
 objectClass: inetOrgPerson
 objectClass: posixAccount
 objectClass: shadowAccount
@@ -69,10 +69,10 @@ loginShell: /bin/bash
 gecos: mauricio
 description: Mauricio" > mauricio.ldif
 
-sudo ldapadd -x -D cn=admin,dc=LDAPS,dc=fam,dc=local -W -f alberto.ldif
-sudo ldapadd -x -D cn=admin,dc=LDAPS,dc=fam,dc=local -W -f fernando.ldif
-sudo ldapadd -x -D cn=admin,dc=LDAPS,dc=fam,dc=local -W -f mauricio.ldif
+sudo ldapadd -x -D cn=admin,dc=ldap,dc=dominio,dc=com -W -f alberto.ldif
+sudo ldapadd -x -D cn=admin,dc=ldap,dc=dominio,dc=com -W -f fernando.ldif
+sudo ldapadd -x -D cn=admin,dc=ldap,dc=dominio,dc=com -W -f mauricio.ldif
 
 #Veririficar que todo esté en orden
-cls
-slapcat
+clear
+sudo slapcat
