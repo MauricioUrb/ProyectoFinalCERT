d4627f820efc6c173f1331f79eaed44cf09a5831
MauricioUrb <maurbina96@hotmail.com>
2021-01-15 20:10:13 -0600

    composer y drush

--- Drupal/drush_drupal.sh
+++ Drupal/drush_drupal.sh
@@ -1,4 +1,3 @@
-sudo apt-get install php php-cli php-common php-curl php-dev php-imap php-json php-ldap php-pgsql php-snmp php-xml -y
 sudo wget -O /usr/bin/composer https://getcomposer.org/composer-stable.phar
 sudo chmod +x /usr/bin/composer
 composer require drush/drush:dev-master
