2e6b0b9f601bf7dc7be49a2f59f14121c2593c2e
MauricioUrb <maurbina96@hotmail.com>
2021-01-15 20:00:12 -0600

    prueba instalación drush y composer

--- Drupal/drush_drupal.sh
+++ Drupal/drush_drupal.sh
@@ -1,6 +1,6 @@
 sudo apt-get install php php-cli php-common php-curl php-dev php-imap php-json php-ldap php-pgsql php-snmp php-xml -y
-sudo wget -O /usr/bin/composer.phar https://getcomposer.org/composer-stable.phar
-sudo chmod +x /usr/bin/composer.phar
+sudo wget -O /usr/bin/composer https://getcomposer.org/composer-stable.phar
+sudo chmod +x /usr/bin/composer
 composer require drush/drush:dev-master
 # A revisar en qué parte se quedará este archivo
 #export PATH="$HOME/vendor/bin:$PATH"
