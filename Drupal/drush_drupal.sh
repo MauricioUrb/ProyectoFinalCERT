cda750ba3704a462d793b98ad5aab657a3debdfc
MauricioUrb <maurbina96@hotmail.com>
2021-01-15 20:33:04 -0600

    drush ok

--- Drupal/drush_drupal.sh
+++ Drupal/drush_drupal.sh
@@ -3,6 +3,7 @@ sudo chmod +x /usr/bin/composer
 composer require drush/drush:dev-master
 # A revisar en qué parte se quedará este archivo
 #export PATH="$HOME/vendor/bin:$PATH"
-sudo export PATH="$HOME/ProyectoFinalCERT/Drupal/vendor/bin:$PATH"
+#export PATH="$HOME/ProyectoFinalCERT/Drupal/vendor/bin:$PATH"
+sudo echo "PATH=$PATH:$HOME/ProyectoFinalCERT/Drupal/vendor/bin" >> ~/.bashrc
+source ~/.bashrc
 #En este punto ya se puede usar drush
-echo "Termino"
