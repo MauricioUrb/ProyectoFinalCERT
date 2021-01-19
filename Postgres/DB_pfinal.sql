01e9b60c254f7be85976f9e84db2ee4e7a73960b
oscar011 <oscar11011996@hotmail.com>
2021-01-18 21:17:59 -0600

    Update DB_pfinal.sql

--- Postgres/DB_pfinal.sql
+++ Postgres/DB_pfinal.sql
@@ -5,18 +5,6 @@ CREATE DATABASE drupaldb WITH OWNER usuario;
 \c drupaldb
 
 --Creación de tablas
-
-CREATE TABLE comentarios
-(
- id_comentarios bigserial NOT NULL,
- comentarios    char(70) NOT NULL,
- id_usuarios    integer NOT NULL,
- id_revision    bigint NOT NULL,
- CONSTRAINT PK_comentarios PRIMARY KEY ( id_comentarios ),
- CONSTRAINT FK_usuarios FOREIGN KEY ( id_usuarios ) REFERENCES usuarios ( id_usuarios ),
- CONSTRAINT FK_revisiones FOREIGN KEY ( id_revision ) REFERENCES revisiones ( id_revision )
-);
-
 CREATE TABLE cvss
 (
  id_cvss     bigserial NOT NULL,
@@ -25,6 +13,26 @@ CREATE TABLE cvss
  enlace_cvss char(159) NOT NULL,
  CHECK (vector_cvss ~* 'AV:[ANLP]\/AC:[HL]\/PR:[NLH]\/UI:[NR]\/S:[UC]\/C:[NLH]\/I:[NLH]\/A:[NLH](\/E:[UPFH])?(\/RL:[OTWU])?(\/RC:[URC])?(\/CR:[LMH])?(\/IR:[LMH])?(\/AR:[LMH])?(\/MAV:[NALP])?(\/MAC:[LH])?(\/MPR:[NLH])?(\/MUI:[NR])?(\/MS:[UC])?(\/MC:[NLH])?(\/MI:[NLH])?(\/MA:[NLH])?'),
  CONSTRAINT PK_cvss PRIMARY KEY ( id_cvss )
+);
+
+CREATE TABLE sitios
+(
+ id_sitios           bigserial NOT NULL,
+ descripción_sitios  char(70) NOT NULL,
+ dir_ip_sitios       text NOT NULL,
+ dependencias_sitios text NOT NULL,
+ url_sitios          char(2083) NOT NULL,
+ CONSTRAINT PK_sitios PRIMARY KEY ( id_sitios )
+);
+
+CREATE TABLE usuarios
+(
+ id_usuarios    serial NOT NULL,
+ nombre_usuario char(70) NOT NULL,
+ correo         char(70) NOT NULL,
+ CHECK (nombre_usuario ~* '[\wüáéíóúñÁÉÍÓÚ]+'),
+ CHECK (correo ~* '[\d\w\.\_\-]+@[\w]+(\.\w[\w]+)+'),
+ CONSTRAINT PK_usuarios PRIMARY KEY ( id_usuarios )
 );
 
 CREATE TABLE hallazgos
@@ -54,24 +62,14 @@ CREATE TABLE revisiones
  CONSTRAINT FK_hallazgos FOREIGN KEY ( id_hallazgos ) REFERENCES hallazgos ( id_hallazgos )
 );
 
-CREATE TABLE sitios
+CREATE TABLE comentarios
 (
- id_sitios           bigserial NOT NULL,
- descripción_sitios  char(70) NOT NULL,
- dir_ip_sitios       text NOT NULL,
- dependencias_sitios text NOT NULL,
- url_sitios          char(2083) NOT NULL,
- CONSTRAINT PK_sitios PRIMARY KEY ( id_sitios )
+ id_comentarios bigserial NOT NULL,
+ comentarios    char(70) NOT NULL,
+ id_usuarios    integer NOT NULL,
+ id_revision    bigint NOT NULL,
+ CONSTRAINT PK_comentarios PRIMARY KEY ( id_comentarios ),
+ CONSTRAINT FK_usuarios FOREIGN KEY ( id_usuarios ) REFERENCES usuarios ( id_usuarios ),
+ CONSTRAINT FK_revisiones FOREIGN KEY ( id_revision ) REFERENCES revisiones ( id_revision )
 );
-
-CREATE TABLE usuarios
-(
- id_usuarios    serial NOT NULL,
- nombre_usuario char(70) NOT NULL,
- correo         char(70) NOT NULL,
- CHECK (nombre_usuario ~* '[\wüáéíóúñÁÉÍÓÚ]+'),
- CHECK (correo ~* '[\d\w\.\_\-]+@[\w]+(\.\w[\w]+)+'),
- CONSTRAINT PK_usuarios PRIMARY KEY ( id_usuarios )
-);
-
 
