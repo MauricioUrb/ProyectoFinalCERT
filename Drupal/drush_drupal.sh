101f76f5fc218ed59c192659f65f6cd3864ca369
MauricioUrb <maurbina96@hotmail.com>
2021-01-19 16:30:47 -0600

    casi instalacion de drupal

--- Drupal/drush_drupal.sh
+++ Drupal/drush_drupal.sh
@@ -1,18 +1,17 @@
-sudo mv composer.phar /usr/bin/composer
+sudo cp composer.phar /usr/bin/composer
 sudo chmod +x /usr/bin/composer
 composer require drush/drush:dev-master
 # A revisar en qué parte se quedará este archivo
-#export PATH="$HOME/vendor/bin:$PATH"
-#export PATH="$HOME/ProyectoFinalCERT/Drupal/vendor/bin:$PATH"
+# por lo mientras se asume que el repo se clonó en el home
 sudo echo "PATH=$PATH:$HOME/ProyectoFinalCERT/Drupal/vendor/bin" >> ~/.bashrc
 source ~/.bashrc
 #En este punto ya se puede usar drush
 # Descarga de drupal
-#composer create-project drupal/recommended-project carpeta
-# o con drush
-mkdir carpeta/destino
-cd carpeta/destino
-drush site-install standard --db-url='mysql://[DB_USER]:[DB_PASS]@[DB_HOST]:[DB_PORT]/[DB_NAME]' --account-name=ADMIN-NAME --account-pass=YOUR-PW --site-name=WEBSITE-NAME --site-mail=WEBSITE@MAIL.COM \
-# composer require drush/drush:^10 ?
+alias tmp="cd /var/www/"
+alias
+sudo composer create-project drupal/recommended-project drupal
+# Instalación de drupal
+#drush site-install standard --db-url='mysql://[DB_USER]:[DB_PASS]@[DB_HOST]:[DB_PORT]/[DB_NAME]' --account-name=ADMIN-NAME --account-pass=YOUR-PW --site-name=WEBSITE-NAME --site-mail=WEBSITE@MAIL.COM \
+
 
 #https://matti.dev/post/setup-install-drupal-9-with-composer-and-drush
