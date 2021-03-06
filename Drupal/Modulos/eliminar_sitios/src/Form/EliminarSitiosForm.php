433eb0013b29ff51db3d715967152fa719ffbc19
oscar011 <oscar11011996@hotmail.com>
2021-03-06 13:08:53 -0600

    boton cancelar

--- Drupal/Modulos/eliminar_sitios/src/Form/EliminarSitiosForm.php
+++ Drupal/Modulos/eliminar_sitios/src/Form/EliminarSitiosForm.php
@@ -7,6 +7,8 @@ namespace Drupal\eliminar_sitios\Form;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * Provides a RSVP Email form.
@@ -39,6 +41,13 @@ class EliminarSitiosForm extends FormBase {
       '#value' => t('Aceptar'),
     );
 
+    $url = Url::fromRoute('sitios_show.content', array());
+    $project_link = Link::fromTextAndUrl('Cancelar', $url);
+    $project_link = $project_link->toRenderable();
+//    $project_link['#attributes'] = array('class' => array('button'));
+    $project_link['#attributes'] = array('class' => array('button', 'button-action', 'button--primary', 'button--small'));
+    $form['Cancelar'] = array('#markup' => render($project_link),);
+
     return $form;
   }
 
@@ -47,16 +56,14 @@ class EliminarSitiosForm extends FormBase {
 	  //se hace la conexion a la base de datos
 	  \Drupal\Core\Database\Database::setActiveConnection('drupaldb_segundo');
 	  $connection = \Drupal\Core\Database\Database::getConnection();
-    //eliminamos primero la referencia del sitio en la tabla dependencias_sitios
 	  $elimina = $connection->delete('dependencias_sitios')
 		  //se agrega la condicion id_sitio
 		  ->condition('id_sitio', $ids)
 		  ->execute();
-    //despues eliminamos la referencia del sitio en la tabla ip_sitios
 	  $elimina = $connection->delete('ip_sitios')
 		  ->condition('id_sitio', $ids)
 		  ->execute();
-	  //por ultimo se elimina el registro en la tabla sitios
+	  // se elimina un registro tabla hallazgos
 	  $update = $connection->delete('sitios')
 		  //Se agregan condiciones
 		  //Donde el id sea
