268355a179a6a7f782d13532ffce4c448d5e6ca9
mauricio.urbina <mauricio.urbina@bec.seguridad.unam.mx>
2021-03-07 00:47:39 -0600

    imagenes en revisiones pentester

--- Drupal/drush_drupal.sh
+++ Drupal/drush_drupal.sh
@@ -138,7 +138,7 @@ cp -r Modulos/* /var/www/drupal/web/modules/.
 /var/www/drupal/vendor/bin/drush en mostrar_imagen
 /var/www/drupal/vendor/bin/drush en eliminar_imagen
 /var/www/drupal/vendor/bin/drush en agregar_imagen
-/var/www/drupal/vendor/bin/drush en edit_revision
+/var/www/drupal/vendor/bin/drush en editar_imagen
 #Hallazgos
 /var/www/drupal/vendor/bin/drush en editar_hallazgos
 /var/www/drupal/vendor/bin/drush en eliminar_hallazgos
