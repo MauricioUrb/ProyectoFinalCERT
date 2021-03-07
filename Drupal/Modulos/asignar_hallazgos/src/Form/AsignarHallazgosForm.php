35c0ee94a9661cec800a57e7f92bde010c28dac5
MauricioUrb <maurbina96@hotmail.com>
2021-03-06 19:48:16 -0600

    test img

--- Drupal/Modulos/asignar_hallazgos/src/Form/AsignarHallazgosForm.php
+++ Drupal/Modulos/asignar_hallazgos/src/Form/AsignarHallazgosForm.php
@@ -31,6 +31,7 @@ class AsignarHallazgosForm extends FormBase{
     global $id_hall;
     $id_hall = $hall_id;
     global $hall_arr;
+    global $cantidadImg;
     //Consulta de la URL del sitio para imprimirlo
     Database::setActiveConnection('drupaldb_segundo');
     $connection = Database::getConnection();
@@ -64,6 +65,8 @@ class AsignarHallazgosForm extends FormBase{
         '#options' => $hallazgos,
         '#required' => TRUE,
       );
+      $cantidadImg = 5;
+      Database::setActiveConnection();
     }else{
       $pos = 0;
       $select = Database::getConnection()->select('hallazgos', 'h');
@@ -85,7 +88,21 @@ class AsignarHallazgosForm extends FormBase{
         '#required' => TRUE,
         '#default_value' => $pos,
       );
+      //Obtener el id_rev_sitio_hall para 
+      $consulta = Database::getConnection()->select('revisiones_hallazgos', 'h');
+      $consulta->fields('h', array('id_rev_sitio_hall'));
+      $consulta->condition('id_rev_sitio',$rev_id);
+      $consulta->condition('id_hallazgo',$hall_id);
+      $id_rev_sitio_hall = $consulta->execute()->fetchCol();
+      Database::setActiveConnection();
+      //Cantidad de imágenes ya agregadas
+      $consulta = Database::getConnection()->select('file_managed', 'f');
+      $consulta->addExpression('MAX(id_rev_sh)','file_managed');
+      $resultado = $consulta->execute()->fetchCol();
+      $cantidadImg = 5 - $resultado[0];
     }
+    Database::setActiveConnection('drupaldb_segundo');
+    $connection = Database::getConnection();
     //Se obtienen valores si ya existen
     $select = Database::getConnection()->select('revisiones_hallazgos', 'h');
     $select->fields('h', array('descripcion_hall_rev'));
@@ -118,9 +135,32 @@ class AsignarHallazgosForm extends FormBase{
       '#required' => TRUE,
       '#default_value' => $recursos_afectador,
     );
-    /*
-    En esta parte se agregará lo correspondiente a las imágenes a agregar
-    */
+    //Imágemes
+    for($i = 1; $i <= $cantidadImg; $i++){
+      $form['grupo'.$i] = array(
+        '#type' => 'fieldset',
+        '#collapsible' => TRUE, 
+        '#collapsed' => FALSE,
+      );
+      $form['grupo'.$i]['img'.$i] =array(
+        '#type' => 'managed_file',
+        '#title' => 'Evidencia ' . $i,
+        '#description' => t('Debe ser formato: jpg, jpeg, png'),
+        '#upload_validators' => array(
+              //se valida que solo sean archivos de imagen
+              'file_validate_extensions' => array('jpg jpeg png'),
+              //se limita su tamaño a 100MB
+              'file_validate_size' => array(1024 * 1024 * 100),
+        ),
+        '#upload_location' => 'public://content/evidencia',
+      );
+      $form['grupo'.$i]['description'.$i] = array(
+        '#type' => 'textfield',
+        '#title' => 'Descripcion evidencia ' . $i,
+        '#size' => 100,
+        '#maxlength' => 100,
+      );
+    }
     $form['impacto'] =array(
       '#type' => 'textfield',
       '#title' => 'Impacto',
@@ -226,6 +266,26 @@ class AsignarHallazgosForm extends FormBase{
     $consulta->condition('id_hallazgo',$id_hallazgo[0]);
     $id_rev_sitio_hall = $consulta->execute()->fetchCol();
     Database::setActiveConnection();
+    //Obtencion de las imagenes del formulario
+    for($i = 1; $i <= $cantidadImg; $i++){
+      $form_file = $form_state->getValue(['grupo'.$i]['imagen'.$i], 0);
+      if($form_file){
+        $file = File::load($form_file[0]);
+        //se guarda en la base de datos file_managed
+        $file->setPermanent();
+        $file->save();
+        //extraer el nombre del archivo subido
+        $file_name = $file->getFilename();
+        //se hace update de esa tabla para agregar referencia a la tabla revisiones
+        $update = $connection->update('file_managed')
+                ->fields(array(
+                    'id_rev_sh' => $id_rev_sitio_hall[0],
+                    'descripcion' => $form_state->getValue(['grupo'.$i]['description'.$i]),
+                ))
+                ->condition('filename', $file_name)
+                ->execute();
+        }
+    }
     //Inserción en la bd de imagenes
     $messenger_service = \Drupal::service('messenger');
     $messenger_service->addMessage($mensaje);
