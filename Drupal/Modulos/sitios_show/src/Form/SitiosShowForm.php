77fe0654aa04e0dd37b4e3616bf3424ece59ccce
AngelLS24 <angls2495@gmail.com>
2021-03-25 10:22:03 -0600

    Descarga de archivo

--- Drupal/Modulos/sitios_show/src/Form/SitiosShowForm.php
+++ Drupal/Modulos/sitios_show/src/Form/SitiosShowForm.php
@@ -111,7 +111,50 @@ class SitiosShowForm extends FormBase{
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $messenger_service = \Drupal::service('messenger');
-    $messenger_service->addMessage(t('The form is working.'));
+    //$messenger_service = \Drupal::service('messenger');
+    //$messenger_service->addMessage(t('The form is working.'));
+
+    //conectar a la otra db
+    \Drupal\Core\Database\Database::setActiveConnection('drupaldb_segundo');
+    $connection = \Drupal\Core\Database\Database::getConnection();
+
+    //se selecciona la base de datos
+    $select = $connection->select('dependencias', 'd');
+    //Se hace un join con tablas necesarias
+    $select ->join('dependencias_sitios', 'ds', 'd.id_dependencia = ds.id_dependencia');
+    $select ->join('sitios', 's', 's.id_sitio = ds.id_sitio');
+    $select ->join('ip_sitios', 'ips', 's.id_sitio = ips.id_sitio');
+    $select ->join('dir_ip', 'ip', 'ip.id_ip = ips.id_ip');
+    //Se especifican las columnas a leer
+    $select->fields('s', array('id_sitio', 'descripcion_sitio', 'url_sitio'))
+	   ->fields('d', array('nombre_dependencia', 'id_dependencia' ))
+	   ->fields('ip', array('dir_ip_sitios', 'id_ip'));
+    //evitar repetidos
+    $select->distinct();
+    $select->orderBy('url_sitio');
+    //Se realiza la consulta
+    $results = $select->execute();
+
+    $valores = array();
+    $valores[0] = ["Descripcion", "URL", "IP", "Dependencia"];
+    $cont = 1;
+    foreach($results as $result){
+	    $valores[$cont] = ["$result->descripcion_sitio", "$result->url_sitio", "$result->dir_ip_sitios", "$result->nombre_dependencia"];
+	    $cont++;
+    }
+
+    $spreadsheet = new Spreadsheet();
+    $worksheet = $spreadsheet->getActiveSheet();
+    $worksheet->fromArray($valores);
+
+    $date = date("Y-m-d_H-i-s");
+    $filename = "$date" . "_sitios.csv";
+
+    $writer1 = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet);
+    $writer1->setSheetIndex(0);   // Select which sheet to export.
+    $writer1->save("public://csv_files/sitios/export/$filename");
+
+    $response = new RedirectResponse("/sites/default/files/csv_files/sitios/export/$filename", 302);
+    $response->send();
   }
 }
