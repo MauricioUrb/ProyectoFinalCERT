4976f6425dc624ce1b24bae2f9b95603cdf4b128
mauricio.urbina <mauricio.urbina@bec.seguridad.unam.mx>
2021-06-11 19:04:49 -0500

    update

--- Drupal/Modulos/eliminar_hallazgos/src/Form/EliminarHallazgosForm.php
+++ Drupal/Modulos/eliminar_hallazgos/src/Form/EliminarHallazgosForm.php
@@ -9,6 +9,7 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 use Drupal\Core\Link;
+use Drupal\Core\Mail\MailInterface;
 
 /**
  * Provides a RSVP Email form.
@@ -27,6 +28,17 @@ class EliminarHallazgosForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, $id_h = NULL) {
     if (in_array('coordinador de revisiones', \Drupal::currentUser()->getRoles())){ 
+      //Se revisa que el sitio esté activo para poder editarlo
+      \Drupal\Core\Database\Database::setActiveConnection('drupaldb_segundo');
+      $connection = \Drupal\Core\Database\Database::getConnection();
+      $select = Database::getConnection()->select('hallazgos', 'h');
+      $select->fields('h', array('activo'));
+      $select->condition('id_hallazgo',$id_h);
+      $results = $select->execute()->fetchCol();
+      \Drupal\Core\Database\Database::setActiveConnection();
+      if(!$results[0]){
+        return array('#markup' => "Este registro no está disponible. Contacta con el administrador.",);
+      }
       global $idh;
       $idh = $id_h;
       $txt = '';
@@ -58,26 +70,74 @@ class EliminarHallazgosForm extends FormBase {
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
 	  global $idh;
+    $messenger_service = \Drupal::service('messenger');
 	  // se hace la conexion a la base de datos
 	  \Drupal\Core\Database\Database::setActiveConnection('drupaldb_segundo');
 	  $connection = \Drupal\Core\Database\Database::getConnection();
-	  // se elimina un registro tabla hallazgos
-	  $update = $connection->delete('hallazgos')
-		  //Se agregan condiciones
+    //Nombre del hallazgo
+    $consulta = Database::getConnection()->select('hallazgos', 'h');
+    $consulta->fields('h', array('nombre_hallazgo_vulnerabilidad'));
+    $consulta->condition('id_hallazgo', $idh);
+    $nombreHallazgo = $consulta->execute()->fetchCol();
+    //Revisiones actualmente activas
+    $consulta = Database::getConnection()->select('actividad', 'a');
+    $consulta->fields('a', array('id_revision'));
+    $consulta->groupBy('id_revision');
+    $consulta->having('MAX(id_estatus) < 4');
+    $revisiones = $consulta->execute()->fetchCol();
+    //Revisiones activas que han asignado este hallazgo
+    $consulta = Database::getConnection()->select('revisiones_hallazgos', 'h');
+    $consulta->join('revisiones_sitios', 'r', 'h.id_rev_sitio = r.id_rev_sitio');
+    $consulta->fields('h', array('id_rev_sitio_hall'));
+    $consulta->condition('id_hallazgo', $idh);
+    $consulta->condition('id_revision', $revisiones, 'IN');
+    $irsh = $consulta->execute()->fetchCol();
+    // se desactiva el registro en la tabla hallazgos
+	  $update = $connection->update('hallazgos')
+		  ->fields(array('activo' => 0))
+      //Se agregan condiciones
 		  //Donde el id sea
 		  ->condition('id_hallazgo', $idh)
 		  // ejecutamos el query
 	  	  ->execute();
-	  // mostramos el mensaje
-	  if($update!=FALSE){
-		  $messenger_service = \Drupal::service('messenger');
-		  $messenger_service->addMessage(t('Se ha actualizado la base de datos'));
-	  } else {
-		  $messenger_service = \Drupal::service('messenger');
-                  $messenger_service->addMessage(t('No existe el registro'));
-	  }
 	  //regresar a la db default
-	  \Drupal\Core\Database\Database::setActiveConnection();
+    \Drupal\Core\Database\Database::setActiveConnection();
+    //Si no se pudo actualizar, no se realiza ninguna otra acción
+    if(!$update){
+      $messenger_service->addMessage(t('No existe el registro.'));
+      $form_state->setRedirectUrl(Url::fromRoute('hallazgos_show.content'));
+    }
+    $mensaje = 'Hallazgo eliminado.';
+    
+    //Si se desactivo con éxito, se procede a borrar de las revisiones activas y enviar el correo
+    if(sizeof($irsh)){
+      \Drupal\Core\Database\Database::setActiveConnection('drupaldb_segundo');
+      $connection = \Drupal\Core\Database\Database::getConnection();
+      $update = $connection->delete('revisiones_hallazgos')
+        ->condition('id_rev_sitio_hall',$irsh,'IN')
+        ->execute();
+      \Drupal\Core\Database\Database::setActiveConnection();
+    }
+    
+    //Notificacion de correo de elminacion de hallazgo
+    $consulta = Database::getConnection()->select('users_field_data', 'r');
+    $consulta->join('user__roles','u','r.uid = u.entity_id');
+    $consulta->fields('r', array('mail'));
+    $consulta->condition('uid',0,'<>');
+    $consulta->condition('mail','admin@example.com','<>');
+    $consulta->condition('roles_target_id',array('coordinador de revisiones','pentester'),'IN');
+    $correos = $consulta->execute()->fetchCol();
+    $to = "";
+    foreach($correos as $mail){
+      $to .= $mail.',';
+    }
+    $to = substr($to, 0, -1);
+    $params['context']['subject'] = "Notificación de eliminación de hallazgo.";
+    $params['context']['message'] = 'Se ha eliminado el hallazgo "'.$nombreHallazgo.'". Si tiene alguna revisión en proceso en el que se haya asignado este hallazgo, este será eliminado.';
+    //$to = 'mauricio@dominio.com,angel@dominio.com';
+    $email = \Drupal::service('plugin.manager.mail')->mail('system', 'mail', $to, $langcode, $params);
+    if(!$email){$mensaje .= " Ocurrió algún error y no se ha podido enviar el correo de notificación.";}
+    $messenger_service->addMessage($mensaje);
     $form_state->setRedirectUrl(Url::fromRoute('hallazgos_show.content'));
   }
 }
